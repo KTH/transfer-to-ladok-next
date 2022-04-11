f7453c28ac88c6637a2953f22c548060795c9812
Carlos Saito <carlos@saito.style>
2022-04-11 21:42:47 +0200

    Refactor 404

--- pages/api/courses/[...assignmentSubmissions].ts
+++ pages/api/courses/[...assignmentSubmissions].ts
@@ -1,22 +1,40 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { withSessionRoute } from "lib/withSession";
+import { getCanvasClient } from "lib/canvasApi";
 
 export default withSessionRoute(sectionsHandler);
 
-async function sectionsHandler(req: NextApiRequest, res: NextApiResponse) {
-  // Only handle /api/courses/:courseId/assignments/:assignmentId/submissions
+function getPathParams(req: NextApiRequest) {
   const [courseId, w1, assignmentId, w2] = req.query
     .assignmentSubmissions as string[];
 
   if (w1 !== "assignments" || w2 !== "submissions") {
-    res.status(404).json({});
-    return;
+    return null;
   }
 
   if (typeof courseId !== "string" || typeof assignmentId !== "string") {
-    res.status(404).json({});
-    return;
+    return null;
   }
+
+  if (req.method !== "GET") {
+    return null;
+  }
+
+  return {
+    courseId,
+    assignmentId,
+  };
+}
+
+async function sectionsHandler(req: NextApiRequest, res: NextApiResponse) {
+  // Only handle /api/courses/:courseId/assignments/:assignmentId/submissions
+  const params = getPathParams(req);
+
+  if (!params) {
+    return res.status(404).json({});
+  }
+
+  const canvas = await getCanvasClient(req);
 
   res.status(200).json({
     message: "Hello",
